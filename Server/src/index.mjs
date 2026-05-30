2b5ac490b8a77698931cc3532952b1a92267d759
vansh1293 <aroravansh.com@gmail.com>
2026-05-30 21:46:32 +0530

    Fix CORS wildcard origin vulnerability that breaks credentials policy

--- Server/src/index.mjs
+++ Server/src/index.mjs
@@ -20,11 +20,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173","*"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
- 
   })
 );
 
